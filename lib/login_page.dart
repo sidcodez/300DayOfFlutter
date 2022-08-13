0a85710ca000536c08d93b8d1223294f03337250
sidcodez <infosidroy11@gmail.com>
2022-08-13 17:42:27 +0600

    Day 3 Completed

---
+++ lib/login_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+
+class LoginPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: Column
+      (children: [
+        Image.asset("assets/images/login_image.png")
+        ],
+      )
+      );
+  }
+}
